87e974f79116bc061aaee1a18d84bf5c6590a87d
vishnujasvin7698 <38651199+vishnujasvin7698@users.noreply.github.com>
2018-04-24 15:12:20 +0530

    Create vowel.c

---
+++ codekata/vowel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int m;
+    if(m='a','e','i','o','u')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+         printf("consonent");
+    }
+    return 0;
+}
