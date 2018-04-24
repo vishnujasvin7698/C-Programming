adc9c65f564836ea2dd49ba393b59d5100dacc07
vishnujasvin7698 <38651199+vishnujasvin7698@users.noreply.github.com>
2018-04-24 15:25:54 +0530

    Create Alpha.c

---
+++ codekata/Alpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int m;
+    if(m='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+         printf("not");
+    }
+    return 0;
+}
