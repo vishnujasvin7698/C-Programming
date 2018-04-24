c0aa9cf740c612a680cbd9b28602562314e79e0b
vishnujasvin7698 <38651199+vishnujasvin7698@users.noreply.github.com>
2018-04-24 14:52:15 +0530

    Create POSorNEG.c

---
+++ codekata/POSorNEG.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=3;
+    if(a>0)
+    {
+        printf("The number is positive",a);
+    }
+    else
+    {
+        printf("INVALID INPUT");
+    }
+    return 0;
+}
