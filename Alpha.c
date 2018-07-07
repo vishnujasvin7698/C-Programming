7ca58b39a02fc4b397c8be7c7bdbe6532499accd
vishnujasvin7698 <38651199+vishnujasvin7698@users.noreply.github.com>
2018-07-07 07:19:46 +0000

    Create Alpha.c

---
+++ Alpha.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int character;
+    printf("Enter the character:");
+        scanf("%ch",&character);
+    if(('A'&&'Z')||('a'&&'z'))
+    {
+        printf("Alphabet");
+        
+    }
+    else
+    {
+        printf("not");
+        return 0;
+    }
+}
