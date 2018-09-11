8f3e11eca3a6afd88ae7dd166cd96567d0f239f9
vishnujasvin7698 <38651199+vishnujasvin7698@users.noreply.github.com>
2018-09-11 10:23:37 +0000

    Create multi5.c

---
+++ multi5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int num,i,n;
+    printf("Enter the number");
+    scanf("%d",&num);
+    for(i=1;i<=5;i++)
+    {
+        n=num*i;
+        printf("\n%d",n);
+     
+    }
+}
